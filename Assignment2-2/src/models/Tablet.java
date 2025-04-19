b9f832c2d81183b11e2b4d572d7e2009e20630b1
Avren-24 <202483890024@nuist.edu.cn>
2025-04-19 21:56:46 +0800

    Update Tablet.java

--- Assignment2-2/src/models/Tablet.java
+++ Assignment2-2/src/models/Tablet.java
@@ -1,4 +1,33 @@
 package models;
 
-public class Tablet {
+public class Tablet extends WearableDevice {
+    private String operatingSystem;
+
+    
+    // Updated constructor to match ComputingDevice parameters
+    public Tablet(String modelName, double price, Manufacturer manufacturer, String id, String processor, int storage, String operatingSystem) {
+        super(modelName, price, manufacturer, id, processor, storage);
+        this.operatingSystem = operatingSystem;
+    }
+
+    // Updated getInsurancePremium method
+
+    public double getInsurancePremium() {
+        return 0.01 * getPrice();
+    }
+
+    public void setOperatingSystem(String operatingSystem) {
+        this.operatingSystem = operatingSystem;
+    }
+    public String getOperatingSystem() {
+        return operatingSystem;
+    }
+    public String toString() {
+        return super.toString() + " Operating System: " + operatingSystem;
+    }
+
+    // New method connectToInternet
+    public String connectToInternet() {
+        return "Connects to the internet via Wi-Fi";
+    }
 }
